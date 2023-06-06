fb1e370efb2cc394831c00cb9b68ed62087101c5
MinotourDmitriy <Kkunkka1@mail.ru>
2023-06-06 21:27:49 +0300

    add new files

---
+++ template-parts/slick-slider.php
@@ -0,0 +1,33 @@
+<main class="page">
+<section class="slide"> 
+		<div class="container">
+
+			<div class="info-sl__slider slider">
+						<?
+							$pict = carbon_get_theme_option('slider_index');
+							if($pict) {
+								$pictIndex = 0;
+								foreach($pict as $item) { 
+									?>
+								<div class="slider__item">
+									<img src="<?php echo wp_get_attachment_image_src($item['slider_img'], 'full')[0];?>" alt="">
+									<? if ( !empty($item['slider_discount'])) { ?>
+										<div class="info-sl__discounts">
+											<? echo $item['slider_discount']; ?>
+										</div>
+									<? } ?>
+									<div class="info-sl__text-new">  
+										<h2><? echo $item['slider_title']; ?></h2>
+										<p>
+											<? echo $item['slider_subtitle']; ?>
+										</p>
+									</div>
+								</div>
+									<?
+									$pictIndex++;
+								}
+							}
+						?>
+			</div>
+		</section>
+</main>
